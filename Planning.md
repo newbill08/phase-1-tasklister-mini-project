3c0ea708f15b44955b5300e2e9519ee4e7380136
joshua <josh.newbill08@hotmail.com>
2023-05-09 15:12:43 -0500

    displays task

---
+++ Planning.md
@@ -0,0 +1,6 @@
+Stage 1
+[] get the form and attach an event listiener to form
+[] get the task from the input
+[] diplay the list
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  console.log("hello world!")
+
+  addEventListeners()
 });
+
+//get the form and add event listioner
+function addEventListeners() {
+  document
+  .getElementById("create-task-form")
+  .addEventListener("submit", handleFormSubmit)
+}
+
+function handleFormSubmit(e){
+  e.preventDefault()
+  const task =  e.target[0].value
+  //console.log(task)
+  displayTask(task)
+}
+
+function displayTask(newTaskValue){
+ const taskUl = document.getElementById("tasks")
+const taskLi = document.createElement("li")
+taskLi.textContent = newTaskValue
+taskUl.appendChild(taskLi)
+}
+
+
+//get the task from the input
+
+
+
+//diplay the list
